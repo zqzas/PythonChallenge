6667199262ef330d4efd50ca71c107556a4117d8
mh <zqzas1@gmail.com>
2013-05-09 13:24:52 +0800

    solution for 1

---
+++ 1/1.py
@@ -0,0 +1,16 @@
+#Python Challenge Prob 1
+#Sulution by zqzas
+#5.9.2013
+
+import string
+
+s = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj"
+
+url = 'map'
+
+trans = string.maketrans(string.lowercase, string.lowercase[2:] + 'ab')
+
+print string.translate(s, trans)
+print string.translate(url, trans)
+
+
